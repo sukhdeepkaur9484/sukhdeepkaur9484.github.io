c96d2e497eae79306847c28cd6d0afe3f1171ff3
sukhdeepkaur9484 <63815215+sukhdeepkaur9484@users.noreply.github.com>
2020-04-16 15:37:44 -0400

    first commit

---
+++ src/script.js
@@ -0,0 +1,33 @@
+/** Add any JavaScript you need to this file. */
+
+$('body').scrollspy({ 
+    target: '.navbar-default',
+    offset: 80
+});
+
+$(window).load(function() {
+    var $container = $('.portfolio');
+    $container.isotope({
+        filter: '*',
+        animationOptions: {
+            duration: 750,
+            easing: 'linear',
+            queue: false
+        }
+    });
+    $('.cat a').click(function() {
+        $('.cat .active').removeClass('active');
+        $(this).addClass('active');
+        var selector = $(this).attr('data-filter');
+        $container.isotope({
+            filter: selector,
+            animationOptions: {
+                duration: 750,
+                easing: 'linear',
+                queue: false
+            }
+        });
+        return false;
+    });
+
+});
